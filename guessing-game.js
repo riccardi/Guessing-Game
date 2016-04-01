93af93d1066f6b7ea1f0e59c891f68e62316c359
riccardi <riccardi.ashley@gmail.com>
2016-04-01 16:36:44 -0400

    Added displayGIFs()

--- guessing-game.js
+++ guessing-game.js
@@ -1,13 +1,6 @@
 var winningNum = generateNumber();
 var numGuesses = 5;
 var prevGuesses = [];
-var winnerGifs = ['<iframe src="//giphy.com/embed/xbASkE3pEK7pC" width="480" height="274" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
-'<iframe src="//giphy.com/embed/l41lWcjB65zASTfGM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
-'<iframe src="//giphy.com/embed/tswHdSIDNfAUE" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'];
-var loserGifs = ['<iframe src="//giphy.com/embed/oIOVkU7Upb3wY" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
-'<iframe src="//giphy.com/embed/TlTxstYNiz0Yg" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
-'<iframe src="//giphy.com/embed/oRFWicT90ngbK" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
-'<iframe src="http://i.imgur.com/n0mcL51.gifv" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'];
 
 $(document).ready(function() {
 	console.log("winning number: " + winningNum);
@@ -42,18 +35,14 @@ function guess(num) {
 		if (isValid(numberGuessed) == "Valid") {
 			var direction = '';
 			var distance = ''
-			if (numberGuessed == winningNum) {
-				$("#hint").attr("disabled","disabled");
-				$("#submit").attr("disabled","disabled");
-				numGuesses--;
-				$("#gif").html('<iframe src="//giphy.com/embed/l41lWcjB65zASTfGM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="http://giphy.com/gifs/nba-golden-state-warriors-steph-curry-2015-nba-playoffs-l41lWcjB65zASTfGM">via GIPHY</a></p>');
+			if (isWinningNumber(numberGuessed)) {
 				return "You are the winner!";
 			} else if (numberGuessed > winningNum) {
 				direction = "Your guess is too high"
 			} else if (numberGuessed < winningNum) {
 				direction = "Your guess is too low"
 			}
-			$("#gif").html('<iframe src="http://i.imgur.com/n0mcL51.gifv" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+			displayGIF();
 			numGuesses--;
 			prevGuesses.push(numberGuessed);
 			$("#num_guesses > span").html(numGuesses);
@@ -63,6 +52,18 @@ function guess(num) {
 		} else if (isValid(numberGuessed) == "Duplicate") {
 			return "You submitted a duplicate guess.";
 		}
+	}
+}
+
+function isWinningNumber(num) {
+	if (numberGuessed == winningNum) {
+		$("#hint").attr("disabled","disabled");
+		$("#submit").attr("disabled","disabled");
+		numGuesses--;
+		displayGIF("winner");
+		return true;
+	} else {
+		return false;
 	}
 }
 
@@ -96,4 +97,22 @@ function giveHint() {
 	var index =Math.round(Math.random()*hintArray.length);
 	hintArray.splice(index,0,winningNum);
 	return "One of these is the winning number:<br />" + hintArray.join(", ");
+}
+
+function displayGIF(player_status) {
+	var winnerGIFs = ['<iframe src="//giphy.com/embed/xbASkE3pEK7pC" width="480" height="274" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+'<iframe src="//giphy.com/embed/l41lWcjB65zASTfGM" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+'<iframe src="//giphy.com/embed/tswHdSIDNfAUE" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'];
+	var loserGIFs = ['<iframe src="//giphy.com/embed/oIOVkU7Upb3wY" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+'<iframe src="//giphy.com/embed/TlTxstYNiz0Yg" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+'<iframe src="//giphy.com/embed/oRFWicT90ngbK" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+'<iframe src="http://i.imgur.com/n0mcL51.gifv" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'];
+
+	if(player_status == "winner") {
+		var index = Math.round(Math.random()*winnerGIFs.length;
+		$("#gif").html(winnerGIFs[index]);
+	} else {
+		var index = Math.round(Math.random()*loserGIFs.length;
+		$("#gif").html(loserGIFs[index]);
+	}
 }
